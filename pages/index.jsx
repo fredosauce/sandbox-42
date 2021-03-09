04e2c97333e366e990a3b9f6a9b7ca0257ecaf76
Fredo Chen <chenfredo@gmail.com>
2021-03-09 03:03:30 -0800

    cleared out some unused styles and simplified layout

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,16 +11,16 @@ import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import Links from '../components/Links';
 import { useInvertedBorderRadius } from '../utils/use-inverted-border-radius';
 
-const openSpring = { type: 'spring', duration: 0.6, bounce: 0.3 };
+const openSpring = { type: 'spring', duration: 0.6, bounce: 0.2 };
 const closeSpring = {
   type: 'spring',
   duration: 0.45,
-  bounce: 0.3,
+  bounce: 0.4,
 };
 
 const Pill = ({ text }) => {
   return (
-    <div class='text-sm py-1 px-3 bg-gray-50 text-gray-700 inline-block font-semibold tech-pill mb-4 rounded-xl'>
+    <div class='text-sm bg-gray-50 text-gray-700 inline-block font-semibold tech-pill mb-4 rounded-md mr-2'>
       {text}
     </div>
   );
@@ -40,7 +40,7 @@ const Overlay = ({ isSelected }) => (
 
 const List = ({ workItems, match }) => {
   return (
-    <ul class='pl-4 flex flex-col space-y-6'>
+    <ul class='flex flex-col space-y-6'>
       {workItems.map((item, index) => {
         // const isSelected =
         const isSelected = match.params.id === item.id;
@@ -49,10 +49,10 @@ const List = ({ workItems, match }) => {
 
         const y = useMotionValue(0);
         return (
-          <li class='work-item ml-4' key={key}>
+          <li class='work-item' key={key}>
             <Overlay isSelected={isSelected} />
             <motion.div
-              className={`overflow-hidden relative work-item-wrapper  border-transparent duration-400 rounded-md ${
+              className={`overflow-hidden relative work-item-wrapper rounded-md ${
                 isSelected && 'open rounded-3xl'
               }`}
               transition={isSelected ? openSpring : closeSpring}
@@ -72,30 +72,31 @@ const List = ({ workItems, match }) => {
                 }`}
                 layout
               >
+                <Link class='close-icon font-base' to='/'>
+                  +
+                </Link>
                 <div class='work-title-wrapper'>
-                  <Link to={'/' + item.id}>
-                    <h1
-                      class='text-xl font-semibold italic cursor-pointer tracking-wider spectral'
-                      dangerouslySetInnerHTML={{ __html: item.title }}
-                    ></h1>
-                  </Link>
+                  <h1
+                    class='text-xl font-semibold cursor-pointer tracking-wider spectral'
+                    dangerouslySetInnerHTML={{ __html: item.title }}
+                  ></h1>
                 </div>
+                <Link class='work-detail-link' to={'/' + item.id}></Link>
               </motion.div>
               <motion.div
                 class='px-8 lg:my-24 work-detail-outer mx-auto flex flex-col-reverse lg:flex-row items-center lg:space-x-8 my-16'
-                transition={{ duration: 0.3 }}
                 layout
               >
                 <motion.div class='text-box'>
                   <h1
-                    class='text-3xl cursor-pointer my-1 spectral inline-block mr-4'
+                    class='text-2xl md:text-3xl cursor-pointer spectral inline-block mr-4'
                     dangerouslySetInnerHTML={{ __html: item.title }}
                   ></h1>{' '}
-                  <p class='text-gray-400 text-xl'>
+                  <p class='text-gray-400 text-lg'>
                     {item.group} &nbsp;{item.year}&nbsp;{' '}
                   </p>
                   <p class='text-base leading-5 mt-4'>{item.blurb}</p>
-                  <div class='mt-6 flex flex-row flex-wrap space-x-5 '>
+                  <div class='mt-6 flex flex-row flex-wrap'>
                     {item.tech.map((techName, index) => (
                       <Pill text={techName} key={techName + index} />
                     ))}
@@ -119,22 +120,13 @@ const HomePage = ({ workItems }) => {
   // console.log('data from homepage: ', data);
 
   return (
-    <div class='flex flex-col mx-auto page-wrapper md:pl-6 md:text-2xl text-xl'>
-      {/* <NavBar navState='home'></NavBar> */}
-      {/* <motion.div
-        class='border-black border-t-2 border-b-2'
-        layoutId='subNavBar'
-        transition={{ duration: 0.35 }}
-        initial={{ opacity: 0, height: 100 }}
-        animate={{ opacity: 1, height: 0 }}
-      ></motion.div> */}
-      {/* <div class='splash-image'></div> */}
+    <div class='flex flex-col mx-auto page-wrapper pl-4 md:text-2xl text-xl'>
       <div class='hero-section py-12  mx-auto'>
         <div class='hero-inner'>
-          <p class=' pl-4 font-medium'>
+          <p class='font-medium'>
             Fredo Chen is a
             <br />
-            <div class='ml-4'>
+            <div class=''>
               frontend engineer
               <br />
               & creative developer
@@ -142,11 +134,31 @@ const HomePage = ({ workItems }) => {
               based in Los Angeles
             </div>
           </p>
+          <p class='font-medium mt-16 '>Connect via</p>
+          <ul class='contact-list'>
+            <li class=''>
+              <a href='https://linkedin.com/in/fredochen' target='_blank'>
+                linkedin
+              </a>
+              ,&nbsp;
+              <a href='https://github.com/fredosauce' target='_blank'>
+                github
+              </a>
+              ,
+            </li>
+            <li class=''></li>
+            <li class=''>
+              or&nbsp;
+              <a href='mailto:chenfredo@gmail.com' target='_blank'>
+                email
+              </a>
+            </li>
+          </ul>
           <div></div>
         </div>
       </div>
       <div class='work-section mx-auto'>
-        <div class=' tracking-wide mt-12 mb-8 pl-4'>Projects</div>
+        <div class=' tracking-wide mt-12 mb-8'>Selected work</div>
         <Router>
           <Route
             path={['/:id', '/']}
@@ -155,18 +167,6 @@ const HomePage = ({ workItems }) => {
             )}
           />
         </Router>
-      </div>
-      <div class='contact-section mx-auto my-24'>
-        <div class='tracking-wide pl-4'>Contact</div>
-        <ul class='contact-list'>
-          <li>
-            <a href='www.linkedin.com/in/fredochen'>linkedin</a>
-          </li>
-          <li>
-            <a href='https://github.com/fredosauce'>github</a>
-          </li>
-          <li></li>
-        </ul>
       </div>
     </div>
   );
